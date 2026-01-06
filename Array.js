531053833b9145a9fee63eb331e75130c469a188
Rachit Sahu <curiousrachit.26@gmail.com>
2026-01-06 23:56:30 +0530

    today solved one more DSA question Max Consecutive Ones

--- Array.js
+++ Array.js
@@ -285,3 +285,25 @@ console.log(shiftAllZeros([3, 4, 6, 0, 6, 0, 3, 86, 0, 98])) */
 // let arr4 = [0, 1, 0, 3, 12]
 // let result6 = removeZeroes(arr4)
 // console.log(result6);
+
+
+// ====================max consecutive ones ===================
+
+
+const maxConsecutive = (nums) => {
+    let currentCount = 0;
+    let maxCount = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] == 1) {
+            currentCount++;
+        } else {
+            maxCount = Math.max(currentCount, maxCount);
+            currentCount=0;
+        }
+    }
+    return Math.max(currentCount, maxCount);
+}
+
+let arr4 = [0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0]
+let result6 = maxConsecutive(arr4)
+console.log(result6);
