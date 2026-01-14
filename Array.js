186919f35d6936f9487e3af7fffa01f9a8b209ff
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-01-14 22:50:00 +0530

    Update Array.js
    solved two sum array problem

--- Array.js
+++ Array.js
@@ -307,3 +307,17 @@ console.log(shiftAllZeros([3, 4, 6, 0, 6, 0, 3, 86, 0, 98])) */
 // let arr4 = [0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0]
 // let result6 = maxConsecutive(arr4)
 // console.log(result6);
+
+
+
+// ====================two sum ==================
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+var twoSum = function (nums, target) {
+  for(let i=0; i<nums.length; i++){
+    for(let j=i+1;j<nums.length; j++){
+        if(nums[i]+nums[j]===target){
+            return [i,j];
+        }
+    }
+  }
