5ecea99bce76ed29646dcdd6d5da1f9fa20f185d
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-01-29 22:57:21 +0530

    Update Array.js
    merged sorted array

--- Array.js
+++ Array.js
@@ -323,6 +323,30 @@ var twoSum = function (nums, target) {
     }
   }
   };
+
+
+
+// 11 Merge Sorted Array
+
+
+var merge = function(nums1, m, nums2, n) {
+let i = m - 1; // last valid element in nums1
+let j = n - 1; // last element in nums2
+let k = m + n - 1; // last position in nums1
+
+
+// Merge from the back
+while (j >= 0) {
+if (i >= 0 && nums1[i] > nums2[j]) {
+nums1[k] = nums1[i];
+i--;
+} else {
+nums1[k] = nums2[j];
+j--;
+}
+k--;
+}
+};
 let arr = [2, 7, 11, 15]
 let tar = 9
 const result = twoSum(arr, tar)
