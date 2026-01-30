b5b89d59b670a8752c16acb31ce43da09eb0c067
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-01-30 22:21:46 +0530

    Add files via upload
    two pointer question

---
+++ Two_pointer&Sliding_window.js
@@ -0,0 +1,15 @@
+
+
+// =============1. Is Subsequence========
+
+var isSubsequence = function(s, t) {
+    let i=0;
+    j=0;
+    while (j<t.length){
+        if(s[i]===t[j]){
+            ++i;
+        }
+        ++j;
+    }
+    return i===s.length;
+};
