b8ad5ec8d62e8204ee772dfe352cd6257d44d4dc
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-02-07 23:27:24 +0530

    Update Stack_and_Queues.js

--- Stack_and_Queues.js
+++ Stack_and_Queues.js
@@ -95,3 +95,52 @@ var isValid = function (s) {
 
     return stack.length===0;
 };
+
+// ==========================min stack======================
+
+var MinStack = function() {
+    this.s=[];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    if(this.s.length===0){
+        this.s.push([val,val])
+    }else{
+        let minval=Math.min(val,this.s[this.s.length-1][1])
+        this.s.push([val,minval]);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.s.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+   return this.s[this.s.length-1][0]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.s[this.s.length-1][1]
+};
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
