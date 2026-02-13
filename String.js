c92372849a79d10ec8e616886075196abce934c4
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-02-13 23:55:05 +0530

    Update String.js

--- String.js
+++ String.js
@@ -393,3 +393,45 @@ var isAnagram = function (s, t) {
     return true;
 
 }
+
+
+
+    
+
+// ==================13  Largest Odd Number in String============
+
+var largestOddNumber = function(num) {
+    let n=num.length-1;
+    while(n>=0){
+        if(num[n]%2===1){
+            return num.substring(0,n+1);
+        }
+        --n;
+    }
+    return "";
+};
+
+
+
+// ==================14 Longest Common Prefix ============
+
+
+var longestCommonPrefix = function(strs) {
+    let x=0;
+    while(x<strs[0].length){
+        let ch=strs[0][x];
+        for(let i=1; i<strs.length; i++){
+            if(ch!=strs[i][x] || x==strs[i].length){
+                return strs[0].substring(0,x);
+            }
+        }
+        x++
+    }
+    return strs[0]
+};
+
+
+
+// ==================14 Longest balanced substring || ============
+
+
