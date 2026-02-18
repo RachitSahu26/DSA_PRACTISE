401075ac64fc1c8de88042baebaa33591b861dd0
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-02-18 23:57:11 +0530

    Update Two_pointers.js

--- Two_pointers.js
+++ Two_pointers.js
@@ -179,3 +179,38 @@ function longestSubstringKDistinct(s, k) {
 
   return maxLen;
 }
+
+
+
+
+
+// ================== 3Max Consecutive Ones III===================
+
+var longestOnes = function(nums, k) {
+    
+  let left = 0;
+  let zeroCount = 0;
+  let maxLen = 0;
+
+  for (let right = 0; right < nums.length; right++) {
+
+    if (nums[right] === 0) {
+      zeroCount++;
+    }
+
+    while (zeroCount > k) {
+      if (nums[left] === 0) {
+        zeroCount--;
+      }
+      left++;
+    }
+
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+
+  return maxLen;
+
+
+};
+
+
