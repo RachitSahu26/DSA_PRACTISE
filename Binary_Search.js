3e4b3077b1ff0ac8a5270f94b77569eff540b7fb
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-02-23 21:11:30 +0530

    Update Binary_Search.js

--- Binary_Search.js
+++ Binary_Search.js
@@ -152,3 +152,24 @@ var search = function(arr, target) {
     return -1;
 };
 
+
+
+// ============== 5. Find K Closest Elements===============
+
+var findClosestElements = function(arr, k, x) {
+    let l = 0;
+    let r = arr.length - k;
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2);
+        if ((arr[m + k] - x) < (x - arr[m])) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    let ans = [];
+    for(let i = l; i < l + k; i++) {
+        ans.push(arr[i]);
+    }
+    return ans;
+};
