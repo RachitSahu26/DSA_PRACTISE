f868bc7c1211198525d54f99ec68647d2233b4ef
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-02-24 22:39:17 +0530

    Update Binary_Search.js

--- Binary_Search.js
+++ Binary_Search.js
@@ -173,3 +173,20 @@ var findClosestElements = function(arr, k, x) {
     }
     return ans;
 };
+
+=================6.find peak element ===============
+
+
+var findPeakElement = function(arr) {
+    let l = 0;
+    let r = arr.length - 1;
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (arr[m] < arr[m + 1]) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    return l;
+};
