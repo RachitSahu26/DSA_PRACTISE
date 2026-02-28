f3ea29123d9b504ea32bb5c54122404a938b6942
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-02-28 23:05:14 +0530

    Update Binary_Search.js

--- Binary_Search.js
+++ Binary_Search.js
@@ -213,3 +213,18 @@ var findMin = function(a) {
         }
     }
 };
+
+=============== 8. Find Peak in a Mountain Array=========
+var peakIndexInMountainArray = function(arr) {
+    let l = 0;
+    let r = arr.length - 1;
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (arr[m + 1] > arr[m]) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    return r;
+};
