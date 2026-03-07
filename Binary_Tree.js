47e0728932449fe41803d3768fc387d715e1539a
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-03-07 23:05:29 +0530

    Add files via upload
    binary tree questions

---
+++ Binary_Tree.js
@@ -0,0 +1,137 @@
+// Problem Statement: 1========================
+//  1. Given the root of a binary tree, return the preorder traversal of its nodes’ values.
+
+
+
+// preOrder-> root ->left->right
+
+// Examples:
+// Example 1:
+// Input: root = [1,null,2,3]
+
+// Output: [1,2,3]
+
+// Explanation:
+
+
+
+
+var preorderTraversal = function (root) {
+    let ans = [];
+    function traversal(curr) {
+        if (!curr) return;
+        ans.push(curr.val);
+        traversal(curr.left);
+        traversal(curr.right);
+    }
+    traversal(root);
+    return ans;
+}
+
+
+//  method 2: Given the root of a binary tree, return the preorder traversal of its nodes’ values.
+// preOrder-> root ->left->right
+// without Recursion
+
+
+var preOrderTraversal = (root) => {
+    if (!root) return [];
+
+    let stack = [root];
+    let ans = []
+    while (stack.length) {
+        let curr = stack.pop();
+        ans.push(curr.val);
+        stack.push(curr.left)
+        curr.left && stack.push(curr.left);
+        curr.right && stack.push(curr.right);
+
+
+    }
+
+    return ans;
+}
+
+// Problem Statement: 2 ========================
+// 2. Given the root of a binary tree, return the inorder traversal of its nodes’ values..
+// Indorder ---->left --->root--->right
+// Examples:
+// Example 1:
+// Input: root = [1,null,2,3]
+
+// Output: [1,3,2]
+
+
+var inorderTraversal = function (root) {
+    let ans = [];
+    function traversal(curr) {
+        if (!curr) return;
+        traversal(curr.left);
+        ans.push(curr.val);
+        traversal(curr.right);
+    }
+    traversal(root);
+    return ans;
+};
+
+
+
+
+// Method Second ---withour recursion 
+
+var inorderTraversal= (root) => {
+    if (!root) return [];
+
+    let stack = [];
+    let ans = [];
+
+    let curr = root;
+
+
+    while (curr || stack.length) {
+        while (curr) {
+            stack.push(curr);
+            curr = curr.left;
+
+        }
+        curr = stack.pop();
+        ans.push(curr);
+        curr = curr.right;
+
+
+
+    }
+
+
+    return ans;
+}
+inorderTraversal()
+
+
+// Problem Statement:
+//  3. Given the root of a binary tree, return the postorder traversal of its nodes’ values..
+// postOrder--->left-->right--->root
+// Examples:
+// Example 1:
+// Input: root = [1,null,2,3]
+
+// Output: [3,2,1]
+
+
+var postOrderTraves = function (root) {
+
+    let ans = [];
+    function traversal(curr) {
+        traversal(curr.left);
+        traversal(curr.right);
+        ans.push(curr.val);
+    }
+
+    traversal(root);
+    return ans;
+}
+
+
+
+
+// =================4. Level Order Traversal - Queue - BFS============
