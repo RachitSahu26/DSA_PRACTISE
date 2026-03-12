64f56c5989130e812346e02ca0910b761e2ea031
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-03-13 00:22:18 +0530

    Update Priority_Queue.js

--- Priority_Queue.js
+++ Priority_Queue.js
@@ -16,4 +16,28 @@ var findKthLargest = function(nums, k) {
 };
 
 
+================ 2. Kth Largest Element in a Stream==============
+var KthLargest = function (k, nums) {
+    this.heap = new MinPriorityQueue();
+    this.k = k;
+
+    for (let i = 0; i < nums.length; i++) {
+        this.add(nums[i]);
+    }
+    return null;
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function (val) {
+    this.heap.enqueue(val)
+
+    if (this.heap.size() > this.k) {
+        this.heap.dequeue();
+    }
+
+    return this.heap.front()
+};
 
