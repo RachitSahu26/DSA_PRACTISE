ad635a50fd2b2544a8d3d3ea3ed36a5f5e4151bf
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-03-22 23:57:27 +0530

    Update Greedy.js

--- Greedy.js
+++ Greedy.js
@@ -81,3 +81,18 @@ var insert = function(arr, x) {
     }
     return ans;
 };
+
+// ============merge interval===========
+
+var merge = function(arr) {
+    arr.sort((a, b) => (a[0] - b[0]));
+    let ans = [arr[0]];
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i][0] <= ans[ans.length - 1][1]){
+            ans[ans.length-1][1] = Math.max(ans[ans.length-1][1], arr[i][1])
+        } else {
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+};
