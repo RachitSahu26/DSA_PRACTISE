29177fb51df1c90746d67c3f9105bae31a2bfe38
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-03-27 22:40:47 +0530

    Update Greedy.js

--- Greedy.js
+++ Greedy.js
@@ -136,3 +136,22 @@ var leastInterval = function(tasks, n) {
     let cycles = (maxFreq - 1) * (n + 1) + countMaxFreq;
     return Math.max(tasks.length, cycles);
 };
+
+// =================gas station==============
+
+var canCompleteCircuit = function (gas, cost) {
+    let currGain = 0;
+    let totalGain = 0;
+    let ans = 0;
+    for (let i = 0; i < gas.length; i++) {
+        let gain = gas[i] - cost[i];
+        currGain += gain;
+        totalGain += gain;
+
+        if (currGain < 0) {
+            ans = i + 1;
+            currGain = 0;
+        }
+    }
+    return totalGain < 0 ? -1 : ans;
+};
