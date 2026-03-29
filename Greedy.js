f156966d6b70d78d617a26c358f84d1f61b6bb00
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-03-29 23:08:14 +0530

    Update Greedy.js

--- Greedy.js
+++ Greedy.js
@@ -173,3 +173,31 @@ var carPooling = function(trips, carCapacity) {
     }
     return true; 
 };
+
+// ======================candy==========
+var candy = function(arr) {
+    let n = arr.length;
+    let ans = n;
+    let i = 1;
+
+    while(i < n){
+        if(arr[i] === arr[i-1]) {
+            ++i;
+            continue;
+        }
+        let up = 0;
+        while(i < n && arr[i] > arr[i-1]){
+            ++up;
+            ans = ans + up;
+            ++i;
+        }
+        let down = 0;
+        while(i < n && arr[i] < arr[i-1]) {
+            ++down;
+            ans += down;
+            ++i;
+        }
+        ans = ans - Math.min(up, down);
+    }
+    return ans;
+};
