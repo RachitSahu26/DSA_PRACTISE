a10b2022e63f6a536af202882afb9094c4dc6a65
Rachit Sahu <129506267+RachitSahu26@users.noreply.github.com>
2026-03-30 21:42:15 +0530

    Update Binary_Search.js

--- Binary_Search.js
+++ Binary_Search.js
@@ -257,3 +257,27 @@ var singleNonDuplicate = function(arr) {
     }
     return arr[l];
 };
+
+
+
+
+// ===========================Find k Closest Elements==============
+    
+var findClosestElements = function(arr, k, x) {
+    let l = 0;
+    let r = arr.length - k;
+    while (l < r) {
+        let m = l + Math.floor((r - l) / 2);
+        if ((arr[m + k] - x) < (x - arr[m])) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    let ans = [];
+    for(let i = l; i < l + k; i++) {
+        ans.push(arr[i]);
+    }
+    return ans;
+};
+   
